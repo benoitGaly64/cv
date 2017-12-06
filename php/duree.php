07106bc3787c1ec97f2319d765565b601aa478c9
benoitGaly64 <galy.benoit.64@gmail.com>
2017-12-06 15:38:08 +0100

    indentation

--- php/duree.php
+++ php/duree.php
@@ -52,4 +52,5 @@ function NbJour($debut, $fin = null)
             break;
     }
 }
+
 ?>
--- php/email.php
+++ php/email.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("HTTP/1.0 404 Not Found");
     exit();
 }
@@ -8,12 +8,12 @@ $email_from = $_POST['emailfrom'];
 $email_content = $_POST['email_content'];
 $email_to = "galy.benoit.64@gmail.com";
 
-$headers = 'From: '.$email_from . "\r\n" .
-    'Reply-To: '.$email_from . "\r\n" .
+$headers = 'From: ' . $email_from . "\r\n" .
+    'Reply-To: ' . $email_from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 
-if(@!mail($email_to, 'contact CV', $email_content, $headers )){
+if (@!mail($email_to, 'contact CV', $email_content, $headers)) {
     header("HTTP/1.0 500 Error");
 }
 
